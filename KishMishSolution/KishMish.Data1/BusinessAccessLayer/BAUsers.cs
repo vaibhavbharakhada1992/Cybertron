6e142fa06633201271cb071db89bdc97eeb8d31e
vaibhavbharakhada1992 <vaibhavbharakhada1992@gmail.com>
2018-06-02 00:57:18 +0530

    delete corrupted data project

---
+++ KishMishSolution/KishMish.Data1/BusinessAccessLayer/BAUsers.cs
@@ -0,0 +1,26 @@
+﻿using KishMish.Data.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KishMish.Data.BusinessAccessLayer
+{
+    public static class BAUsers
+    {
+
+        public static bool BA_CheckUserExistance(string emailId)
+        {
+            bool ret = false;
+            ret = DAUsers.DA_CheckUserExistance(emailId);
+            return ret;
+        }
+
+        public static tb_users BA_AuthenticateLoginDetails(string emailId,string password)
+        {
+            tb_users user = DAUsers.DA_AuthenticateLoginDetails(emailId, password);
+            return user;
+        }
+            
+    }
+}
